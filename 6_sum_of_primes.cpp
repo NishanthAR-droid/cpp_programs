b1d391a802a12961c0ce46be7fad6df589a18704
NishanthAR-droid <85539246+NishanthAR-droid@users.noreply.github.com>
2021-07-20 12:40:06 +0530

    Add files via upload

---
+++ 6_sum_of_primes.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int prime(int num);
+
+int main()
+{
+	int num,i;
+	cout<<"Enter a psitive number\n";
+	cin>>num;
+	for(i=2;i<=num/2;i++)
+	{
+		if(prime(i))
+		{
+			if(prime(num-i))
+			cout<<num<<"="<<i<<"+"<<(num-i)<<endl;
+		}
+	}
+	return 0;
+}
+
+int prime(int num)
+{
+	int i;
+	int flag=1;
+	for(i=2;i<=num/2;i++)
+	{
+		if(num%i==0)
+		{
+			flag=0;
+		}
+	}
+	return flag;
+}
